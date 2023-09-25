94ed697fe1c4547ad4f4d8e84b1414e493754ad5
Jakey Wakey <askjake331@gmail.com>
2023-09-25 08:13:41 -0400

    gotta get the girls

--- app/entries/[entryId]/page.tsx
+++ app/entries/[entryId]/page.tsx
@@ -7,10 +7,9 @@ async function Page({ params }: any): Promise<React.JSX.Element>  {
   const response = await fetch('http://127.0.0.1:3000/api/entries/read-all')
   const data: IPost[] = await response.json()
   const post = data.filter((value: IPost) => value._id === params.entryId)[0]
-
   return (
     <div className='card__container--full relative'>
-      <span className='text-primary underline absolute left-2 text-xs font-bold italic hover:text-black top-1'>&larr; <Link href={ post.category }>View More In { getLinkInfo(post.category) }</Link></span>
+      <span className='text-primary underline absolute left-2 text-xs font-bold italic hover:text-black top-1'>&larr; <Link href={ post?.category ?? '/' }>View More In { getLinkInfo(post?.category) }</Link></span>
       { post.title ? <Post
         post={ post }
       /> : null }
